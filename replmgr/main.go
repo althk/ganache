97dc4c020da3a2e168dac149e599d347e9bfff1b
Harish Kotha <althk@users.noreply.github.com>
2022-06-09 13:02:11 +0530

    fix copy pasta error in log statements

--- replmgr/main.go
+++ replmgr/main.go
@@ -17,7 +17,7 @@ import (
 	hpb "google.golang.org/grpc/health/grpc_health_v1"
 )
 
-var port = flag.Int("port", 0, "cache server port, defaults to 0 which means any available port")
+var port = flag.Int("port", 0, "repl mgr port, defaults to 0 which means any available port")
 var debug = flag.Bool("debug", false, "enable debug logging")
 
 func main() {
@@ -40,6 +40,6 @@ func main() {
 	pb.RegisterReplServer(s, &replmgr{
 		mq: ipmq.New(),
 	})
-	log.Info().Msgf("Running repl server on %v", lis.Addr().String())
+	log.Info().Msgf("Running repl mgr on %v", lis.Addr().String())
 	s.Serve(lis)
 }
