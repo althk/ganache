3e460f600051bee494a450b3702cd146b30a48bb
Harish Kotha <althk@users.noreply.github.com>
2022-07-06 22:07:45 +0530

    add grpc keepalive config to all servers, and to the cfe client for slight performance improvement

--- cfe/internal/server/server.go
+++ cfe/internal/server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	cspb "github.com/althk/ganache/cacheserver/proto"
 	"github.com/althk/ganache/cfe/internal/service"
@@ -12,8 +13,15 @@ import (
 	resolverv3 "go.etcd.io/etcd/client/v3/naming/resolver"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/keepalive"
 	"google.golang.org/grpc/resolver"
 )
+
+var kacp = keepalive.ClientParameters{
+	Time:                8 * time.Second, // send pings every 10 seconds if there is no activity
+	Timeout:             2 * time.Second, // wait 1 second for ping ack before considering the connection dead
+	PermitWithoutStream: true,            // send pings even without active streams
+}
 
 func New(tlsCfg *grpcutils.TLSConfig, etcdSpec, csResolverPrefix string, shardCount int) (*service.CFE, error) {
 	r, err := etcdResolver(etcdSpec)
@@ -35,7 +43,12 @@ func New(tlsCfg *grpcutils.TLSConfig, etcdSpec, csResolverPrefix string, shardCo
 func getCacheCli(creds credentials.TransportCredentials, r resolver.Builder, cacheResolverPrefix string, shardNum int) (cspb.CacheClient, error) {
 	ep := strings.Join([]string{"etcd://", cacheResolverPrefix, fmt.Sprint(shardNum)}, "/")
 	log.Info().Msgf("Build cacheserver client for %v", ep)
-	conn, err := grpc.Dial(ep, grpc.WithResolvers(r), grpc.WithTransportCredentials(creds))
+	conn, err := grpc.Dial(
+		ep,
+		grpc.WithResolvers(r),
+		grpc.WithTransportCredentials(creds),
+		grpc.WithKeepaliveParams(kacp),
+	)
 	if err != nil {
 		return nil, err
 	}
--- utils/grpc/grpcutils.go
+++ utils/grpc/grpcutils.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"time"
 
 	grpczerolog "github.com/grpc-ecosystem/go-grpc-middleware/providers/zerolog/v2"
 	middleware "github.com/grpc-ecosystem/go-grpc-middleware/v2"
@@ -17,8 +18,22 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/health"
 	hpb "google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/keepalive"
 	"google.golang.org/grpc/reflection"
 )
+
+var kaep = keepalive.EnforcementPolicy{
+	MinTime:             5 * time.Second, // If a client pings more than once every 5 seconds, terminate the connection
+	PermitWithoutStream: true,            // Allow pings even when there are no active streams
+}
+
+var kasp = keepalive.ServerParameters{
+	MaxConnectionIdle:     15 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
+	MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
+	MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
+	Time:                  5 * time.Second,  // Ping the client if it is idle for 5 seconds to ensure the connection is still active
+	Timeout:               2 * time.Second,  // Wait 1 second for the ping ack before assuming the connection is dead
+}
 
 type TLSConfig struct {
 	CertFilePath     string
@@ -122,6 +137,8 @@ func GetGRPCServerOpts(tlsCfg *TLSConfig) ([]grpc.ServerOption, error) {
 	return []grpc.ServerOption{
 		getServerInterceptorChain(),
 		grpc.Creds(creds),
+		grpc.KeepaliveEnforcementPolicy(kaep),
+		grpc.KeepaliveParams(kasp),
 	}, nil
 }
 
@@ -138,6 +155,7 @@ func NewGRPCServer(grpcCfg *GRPCServerConfig) (*grpc.Server, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	s := grpc.NewServer(serverOpts...)
 
 	// register other servers
